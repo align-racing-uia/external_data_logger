238a464c27640480a26a5ee10a89d59d116e13d5
snenyl <simoneriker@protonmail.com>
2021-08-01 21:20:22 +0200

    Directory cleanup, and added title to README.md

--- AR20_DataLogger/Code V1.1/sketch_feb17a/Config.h
+++
@@ -1,8 +0,0 @@
-  const int tempSensor1Pin = 0;
-  const int tempSensor2Pin = 0;
-  const int tempSensor3Pin = 0;
-  const int pressureSensor1Pin = 0;
-  const int pressureSensor1Pin = 0;
-  const int pressureSensor1Pin = 0;
-
- 
